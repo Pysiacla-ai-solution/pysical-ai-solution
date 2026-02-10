25ae32a983b50398ab5f8dc1dbcfc52e5f45b3e9
lsy010857 <67821048+lsy010857@users.noreply.github.com>
2026-02-10 21:42:37 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -23,11 +23,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 import os
 os.environ["OMP_NUM_THREADS"] = "1"
-os.environ["MKL_NUM_THREADS"] = "1"
-os.environ["OPENBLAS_NUM_THREADS"] = "1"
-os.environ["VECLIB_MAXIMUM_THREADS"] = "1"
-os.environ["NUMEXPR_NUM_THREADS"] = "1"
-#import openai
+
 
 # 0) 환경 변수 로드 (.env)
 
